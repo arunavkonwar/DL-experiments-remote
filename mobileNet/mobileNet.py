3abf08635998b2c527dbd156837c8503f7ac5084
arunav konwar <akonwar@gantry.irisa.fr>
2018-04-10 18:47:15 +0200

    initial commit

---
+++ mobileNet/mobileNet.py
@@ -0,0 +1,102 @@
+def vgg16():
+	import keras
+	from keras.models import Sequential
+	from keras.layers import Activation
+	from keras.layers.core import Dense, Flatten
+	from keras.optimizers import Adam
+	from keras.metrics import categorical_crossentropy
+	from keras.layers.normalization import BatchNormalization
+	from keras.layers.convolutional import *
+	import matplotlib.pyplot as plt
+	from keras.utils import plot_model 
+
+
+	vgg16_model = keras.applications.mobilenet.MobileNet(input_shape=None, alpha=1.0, depth_multiplier=1, dropout=1e-3, include_top=True, weights='imagenet', input_tensor=None, pooling=None, classes=1000)
+
+	model = Sequential()
+	for layer in vgg16_model.layers:
+		model.add(layer)
+	
+	model.layers.pop()
+	
+	model.add(Dense(2, activation=None))
+	
+	for layer in model.layers:
+		layer.trainable = True
+				
+	model.summary()
+	return model
+
+
+
+
+if __name__ == "__main__":
+	import os
+	import numpy as np
+	from keras import optimizers
+	from keras.models import Sequential
+	from keras.models import load_model
+	from keras.layers import Activation
+	from keras.layers.core import Dense, Flatten
+	from keras.optimizers import Adam
+	from keras.metrics import categorical_crossentropy
+	import matplotlib.pyplot as plt
+	import h5py
+	from keras.utils import plot_model
+	#from keras.callbacks import ModelCheckpoint
+	#import utils
+	#import models
+	import time
+	from keras.callbacks import ModelCheckpoint
+
+	np.random.seed(7) # for reproducibility
+
+
+
+	batch_size = 14
+
+	#model = load_model('vgg16_edit.h5')
+	model = vgg16()
+	print('loading model weights')
+	model.load_weights('trained_model_weights.h5')
+
+	y_filename ='/udd/akonwar/code/deep-learning-for-visual-servoing/data/data.txt'
+	y_data = np.loadtxt(y_filename, delimiter='  ', usecols=[0,1])
+
+	y_data_train = y_data[:]
+
+	#########################################
+
+	h5f = h5py.File('/udd/akonwar/code/deep-learning-for-visual-servoing/images_in_h5_format.h5','r')
+	x_data_train = h5f['dataset_1'][:]
+
+
+
+	# ======================================================================                     
+	# Configure the training process:
+	print('Preparing training ...')
+	#adam = Adam(lr=0.0001, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
+	#adam = Adam(lr=0.0001)
+	adam = Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
+	model.compile(optimizer=adam, loss='mean_squared_error', metrics=['accuracy'])
+
+	epochs=10
+	# Train:
+	print('Start training ...')
+	start = time.time()
+	history = model.fit(x = x_data_train, y = y_data_train,
+		  epochs=10,
+		  batch_size=batch_size, validation_split = 0.20, shuffle = True, verbose = 1)  
+		  #By setting verbose 0, 1 or 2 you just say how do you want to 'see' the training progress for each epoch.
+	end = time.time()
+	print ("Model took %0.2f seconds to train"%(end - start))
+
+
+	model.save_weights('trained_model_weights.h5')
+	#model.save('trained_model.h5')
+
+
+
+
+
+
