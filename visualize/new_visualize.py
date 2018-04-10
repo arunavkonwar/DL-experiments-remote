78fe98199cd54441f7250bac70da45ced5297ddd
Arunav Konwar <arunavkonwar@gmail.com>
2018-04-10 19:49:23 +0200

    Update new_visualize.py

--- visualize/new_visualize.py
+++ visualize/new_visualize.py
@@ -1,1 +1,69 @@
-yay
+#https://gist.github.com/stared/dfb4dfaf6d9a8501cd1cc8b8cb806d2e
+
+import keras
+from keras.datasets import mnist
+from keras.utils import to_categorical
+from keras.models import Sequential
+from keras.layers import Flatten, Dense, Activation
+
+import numpy as np
+from matplotlib import pyplot as plt
+from IPython.display import clear_output	
+
+
+# data loading
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+
+# data preprocessing
+Y_train = to_categorical(y_train)
+Y_test = to_categorical(y_test)
+X_train = X_train.reshape(-1, 28, 28, 1).astype('float32') / 255.
+X_test = X_test.reshape(-1, 28, 28, 1).astype('float32') / 255.
+
+
+# just logistic regression, to keep it simple and fast
+
+model = Sequential()
+
+model.add(Flatten(input_shape=(28, 28, 1)))
+model.add(Dense(10))
+model.add(Activation('softmax'))
+
+model.compile(optimizer='rmsprop',
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+              
+
+# in this static viewer it is not obvious,
+# but this plot grows step by step
+
+history = model.fit(X_train, Y_train,
+          epochs=50,
+          validation_data=(X_test, Y_test),
+          verbose=1)
+          
+print(history.history.keys()) 
+
+plt.figure(1)  
+
+# summarize history for accuracy  
+
+plt.subplot(211)  
+plt.plot(history.history['acc'])  
+plt.plot(history.history['val_acc'])  
+plt.title('model accuracy')  
+plt.ylabel('accuracy')  
+plt.xlabel('epoch')  
+plt.legend(['train', 'test'], loc='upper left')  
+
+# summarize history for loss  
+
+plt.subplot(212)  
+plt.plot(history.history['loss'])  
+plt.plot(history.history['val_loss'])  
+plt.title('model loss')  
+plt.ylabel('loss')  
+plt.xlabel('epoch')  
+plt.legend(['train', 'test'], loc='upper left')  
+#plt.show()  
+plt.savefig('visualize.png')
